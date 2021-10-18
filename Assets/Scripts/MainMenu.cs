bef84955edea2dabdfe8530b5134178f3f8ea436
nullsoftware <tumyrthebot@gmail.com>
2021-10-18 22:51:34 +0300

    Add project files.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnStartButtonClick()
+    {
+        Debug.Log("Start game requested.");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnAboutButtonClick()
+    {
+        Debug.Log("About page requested.");
+    }
+
+    public void OnExitClick()
+    {
+        Debug.Log("Exit requested.");
+        Application.Quit();
+    }
+}
